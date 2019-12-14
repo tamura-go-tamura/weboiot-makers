af39178af24e75134f2276855075b48ab5f5d8cc
TAKAHASHI YUSUKE <thys.60918@gmail.com>
2019-12-15 04:11:35 +0900

    fix

---
+++ linebot/majinkoTV.py
@@ -0,0 +1,78 @@
+from flask import Flask,request,abort
+from linebot import LineBotApi,WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent,TextMessage,TextSendMessage,BeaconEvent
+import os
+import motor
+import acc
+
+app=Flask(__name__)
+#環境変数の取得
+YOUR_CHANNEL_ACCESS_TOKEN=""
+YOUR_CHANNEL_SECRET=""
+
+line_bot_api=LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler=WebhookHandler(YOUR_CHANNEL_SECRET)
+
+@app.route("/callback",methods=["POST"])
+def callback():
+    signature=request.headers["X-Line-Signature"]
+
+    body=request.get_data(as_text=True)
+    app.logger.info("Request body"+body)
+
+    try:
+        handler.handle(body,signature)
+    except InvalidSignatureError:
+        abort(400)
+    return "OK"
+
+
+
+@handler.add(MessageEvent,message=TextMessage)
+def handle_message(event):
+    text = event.text
+
+    if text == "あけ":
+        motor.motor("open")
+
+        profile = line_bot_api.get_profile(event.source.user_id)
+        line_bot_api.push_message("U90270fbcc310d31bb0c7bdbaa1e4b01c",
+            TextSendMessage(text="{}が開けました".format(profile.display_name)))
+        
+        acc.Sensing_acceleration()
+
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+
+    elif text == "しめ":
+        motor.motor("close")
+        acc.Sensing_acceleration()
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+
+    else:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+
+
+
+@handler.add(BeaconEvent)
+def handle_beacon(event):
+    #print(event.source.userid)
+    motor.motor("open")
+
+    profile = line_bot_api.get_profile(event.source.user_id)
+    line_bot_api.push_message("U90270fbcc310d31bb0c7bdbaa1e4b01c",
+        TextSendMessage(text="{}が開けました".format(profile.display_name)))
+
+    acc.Sensing_acceleration()
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(
+            text='Got beacon event. hwid={}, device_message(hex string)={}, event_type={}'.format(
+                event.beacon.hwid, event.beacon.dm, event.beacon.type)))
+
+
+if __name__=="__main__":
+    port=int(os.getenv("PORT",5000))
+    app.debug=True
+    app.run()#host="0.0.0.0",port=port)
